9b106122bbfdca8d258c5ab0676ea9dcb40f7201
Monica Jianu <monica@ebi.ac.uk>
2018-10-22 16:39:50 +0100

    Added descriptive class names to SpeciesCard to aid testing. Added more conditional rendering to handle optional props. Updated prop types.

--- src/SpeciesCard.js
+++ src/SpeciesCard.js
@@ -4,28 +4,37 @@ import EbiSpeciesIcon from 'react-ebi-species'
 
 const SpeciesCard = ({iconSrc, description, content}) => {
   return (
-    <div className="column column-block text-center combo" style={{marginBottom:0, paddingBottom: `25px`}}>
-      <span className="large-species-icon" style={{fontSize: `600%`}}>
+    <div className="column column-block text-center combo card" style={{marginBottom:0, paddingBottom: `25px`}}>
+      <span className="species-icon" style={{fontSize: `600%`}}>
         <EbiSpeciesIcon species={iconSrc}/>
       </span>
 
-      <h5 className="species">{description}</h5>
       {
-        content.map((item) =>
-          item.url ?
-            <p style={{marginBottom: `0.3rem`}} key={item.text}><a href={item.url} key={item.text}>{item.text}</a></p> :
-            <p style={{marginBottom: `0.3rem`}} key={item.text}>{item.text}</p>
-        )
+        description && <h5 className="species-name">{description}</h5>
+      }
+
+      {
+        content &&
+          <span className="content">
+            {
+              content.map((item) =>
+                item.url ?
+                  <p id="url" style={{marginBottom: `0.3rem`}} key={item.text}><a href={item.url} key={item.text}>{item.text}</a></p> :
+                  <p id="text" style={{marginBottom: `0.3rem`}} key={item.text}>{item.text}</p>
+              )
+            }
+          </span>
       }
     </div>
   )
 }
 
 SpeciesCard.propTypes = {
-  iconSrc: PropTypes.string,
+  iconSrc: PropTypes.string.isRequired,
   description: PropTypes.string,
   content: PropTypes.arrayOf(PropTypes.shape({
-    text: PropTypes.string
+    text: PropTypes.string.isRequired,
+    url: PropTypes.string
   }))
 }
 
