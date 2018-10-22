7d0a558f765bfa2a999aab962975e1905e965fdc
Monica Jianu <monica@ebi.ac.uk>
2018-10-22 12:39:35 +0100

    Added first version of a container component which fetches data from a JSON endpoint and renders Species cards. The cards contain EBIReactSpecies icons as well as the content included in the payload.

--- src/CardContainer.js
+++ src/CardContainer.js
@@ -1,21 +1,85 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SpeciesCard from './SpeciesCard'
+import URI from 'urijs'
 
 class CardContainer extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      data: null,
+      loading: true,
+      error: null
+    }
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true })
+
+    const url = URI(this.props.resource, this.props.host).toString()
+    const response = await fetch(url)
+
+    try {
+      if (!response.ok) {
+        throw new Error(`${url} => ${response.status}`)
+      }
+
+      this.setState({
+        data: await response.json(),
+        loading: false,
+        error: null
+      })
+    } catch(e) {
+      this.setState({
+        data: null,
+        loading: false,
+        error: {
+          description: `There was a problem communicating with the server. Please try again later.`,
+          name: e.name,
+          message: e.message
+        }
+      })
+    }
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({
+      error: {
+        description: `There was a problem rendering this component.`,
+        name: error.name,
+        message: `${error.message} – ${info}`
+      }
+    })
   }
 
   render() {
+    const { data, loading } = this.state
+
     return (
-      <SpeciesCard/>
+      loading ?
+        <p> Loading, please wait...</p> :
+        data && data.length > 0 ?
+          <div id="by-species">
+            <div className="row small-up-2 medium-up-3">
+              {data.map((species) =>
+                <SpeciesCard
+                  species={species.iconSrc}
+                  description={species.iconDescription}
+                  content={species.content}
+                  key={species.iconSrc}
+                />
+              )}
+            </div>
+          </div> :
+          <p> There are no species.</p>
     )
   }
 }
 
 CardContainer.propTypes = {
-  atlasUrl: PropTypes.string
+  host: PropTypes.string,
+  resource: PropTypes.string
 }
 
 export default CardContainer
--- src/SpeciesCard.js
+++ src/SpeciesCard.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import EbiSpeciesIcon from 'react-ebi-species'
 
-const SpeciesCard = () => {
+const SpeciesCard = ({species, description, content}) => {
   return (
-    <p>SpeciesCard content</p>
+    <div className="column column-block text-center combo">
+      <span className="large-species-icon">
+        <EbiSpeciesIcon species={species}/>
+      </span>
+
+      <h5 className="species">{description}</h5>
+      {
+        content.map((item) =>
+          item.url ?
+            <p><a href={item.url} key={item.text}>{item.text}</a></p> :
+            <p>{item.text}</p>
+        )
+      }
+
+    </div>
   )
 }
 
 SpeciesCard.propTypes = {
+  species: PropTypes.string,
+  description: PropTypes.string,
+  content: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string
+  }))
 }
 
 export default SpeciesCard
