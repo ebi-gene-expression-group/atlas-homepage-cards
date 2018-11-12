8cc75a6f786583a892a297b37ec5c771a560954a
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-12 11:18:18 +0000

    Reformat ImageCard for HCA featured experiments…
    as a React component with showResults state to hide the experiments more than five.

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,33 +1,63 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ImageCard = ({iconSrc, iconDescription, content}) => {
-  return (
-    <div className={`column column-block text-center combo card`} style={{marginBottom:0, paddingBottom: `25px`}}>
-      {
-        iconDescription && <h5 className="species-name">{iconDescription}</h5>
-      }
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props)
 
-      <span className={`species-icon`} style={{fontSize: `800%`}}>
-        <img id={`icon`} alt={iconDescription} src={iconSrc}/>
-      </span>
+    this.state = {
+      showResults: false
+    }
+    this.onClick = this.onClick.bind(this)
+  }
+ 
+  onClick(){
+    this.setState({
+      showResults: !this.state.showResults
+    })
+  }
 
-      {
-        content &&
-          <span className="content">
-            {
-              content.map((item) =>
-                item.url ?
-                  <p id={`url`} style={{marginBottom: `0.3rem`}} key={item.text}><a href={item.url} key={item.text}>{item.text}</a></p> :
-                  <p id={`text`} style={{marginBottom: `0.3rem`}} key={item.text}>{item.text}</p>
-              )
-            }
-          </span>
-      }
-    </div>
-  )
+  render(){
+    var {iconSrc, iconDescription, content} = this.props
+    return (
+      <div className={`column column-block text-center combo card`} style={{marginBottom:0, paddingBottom: `25px`}}>
+        {
+          iconDescription && <h5 className="species-name">{iconDescription}</h5>
+        }
+
+        <span className={`species-icon`} style={{fontSize: `800%`}}>
+          <img id={`icon`} alt={iconDescription} src={iconSrc}/>
+        </span>
+
+        {
+          content &&
+            <ul className="show_more" style={{listStyle:`none`, paddingLeft:`0`, marginLeft:`0`}}>
+              {
+                content.map((item, idx) => { 
+                  return(
+                   item.url ?
+                    idx<5 || (idx>=5 && this.state.showResults) ?
+                    <li id={`url`} style={{marginBottom: `0.3rem`}} key={item.text}><a href={item.url} key={item.text}>{item.text}</a></li> :
+                    null :
+                    <li id={`text`} style={{marginBottom: `0.3rem`}} key={item.text}>{item.text}</li>
+                  )
+                })
+              }
+              {
+                content.length>=5 ?
+                    this.state.showResults?
+                    <button className="button small hide_button" onClick={this.onClick}> Hide…</button>
+                    :
+                    <button className="button small show_button" onClick={this.onClick}> See more…</button>
+                  : 
+                 null
+              }
+            </ul>
+        }
+      </div>
+    )
+  }
 }
-
 ImageCard.propTypes = {
   iconSrc: PropTypes.string.isRequired,
   iconDescription: PropTypes.string,
