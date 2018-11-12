a6011acbfa5228b48239765166b71d3c7037a438
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-12 11:59:43 +0000

    Add test for display limit within 5 items

--- __test__/ImageCard.test.js
+++ __test__/ImageCard.test.js
@@ -23,7 +23,7 @@ describe(`ImageCard`, () => {
     expect(wrapper.find(`.content`).exists()).toBe(false)
   })
 
-  test(`does not render non-existent URLs`, () => {
+  test(`does not render non-existent descriptions`, () => {
     const content = [
       {
         text: `120 experiments`
@@ -38,11 +38,9 @@ describe(`ImageCard`, () => {
     const speciesName = `Mus musculus`
 
     const wrapper = shallow(<ImageCard iconSrc={speciesName} content={content} description={speciesName}/>)
-    expect(wrapper.find(`.species-name`).exists()).toBe(true)
-    expect(wrapper.find(`.species-name`).text()).toBe(speciesName)
+    expect(wrapper.find(`.species-name`).exists()).toBe(false)
 
     const contentWrapper = wrapper.find(`.content`)
-
     expect(contentWrapper.exists()).toBe(true)
     expect(contentWrapper.find(`#text`)).toHaveLength(3)
     expect(contentWrapper.find(`#url`).exists()).toBe(false)
@@ -60,5 +58,37 @@ describe(`ImageCard`, () => {
     const urlsWrapper = wrapper.find(`.content #url`)
 
     expect(urlsWrapper).toHaveLength(1)
+  })
+
+  test(`displays upto five experiments be default`, () => {
+    const content = [
+      {
+        text: `1 experiments`
+      },
+      {
+        text: `2 baseline experiment`
+      },
+      {
+        text: `3 differential experiments`
+      },
+      {
+        text: `4 differential experiments`
+      },
+      {
+        text: `5 differential experiments`
+      },
+      {
+        text: `6 differential experiments`
+      }
+    ]
+    const speciesName = `Mus musculus`
+
+    const wrapper = shallow(<ImageCard iconSrc={speciesName} content={content} description={speciesName}/>)
+    expect(wrapper.find(`#text`)).toHaveLength(5)
+    wrapper.setState({ showResults: true })
+    wrapper.update()
+    expect(wrapper.find(`#text`)).toHaveLength(content.length)
+
+
   })
 })
