13a5a9cb53488c1feb671a3355d9087f61a5bc7d
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-11-13 20:48:09 +0000

    Add description link to species icon [Completes #161749807]

--- src/cards/SpeciesCard.js
+++ src/cards/SpeciesCard.js
@@ -4,44 +4,46 @@ import EbiSpeciesIcon from 'react-ebi-species'
 
 import cardPropTypes from './CardPropTypes'
 
-const SpeciesCard = ({iconSrc, description, content}) => {
-  return (
-    <div className={`column column-block text-center combo card`} style={{marginBottom:0, paddingBottom: `25px`}}>
-      <span className={`species-icon`} style={{fontSize: `600%`}}>
-        <EbiSpeciesIcon species={iconSrc}/>
-      </span>
-
-      {
-        description &&
-        <h5 className={`species-name`}>
-          {
-            description.url ?
-              <a className={`species-url`} href={description.url}>{description.text}</a> :
-              description.text
-          }
-        </h5>
-      }
-
-      {
-        content &&
-        <span className={`content`}>
-          {
-            content.map((item) =>
-              item.url ?
-                <p className={`url`} style={{marginBottom: `5px`}} key={item.text}>
-                  <a href={item.url}>{item.text}</a>
-                </p> :
-                <p className={`text`} style={{marginBottom: `5px`}} key={item.text}>{item.text}</p>
-            )
-          }
+const SpeciesCard = ({iconSrc, description, content}) =>
+  <div className={`column column-block text-center combo card`} style={{marginBottom:0, paddingBottom: `25px`}}>
+    {
+      description && description.url ?
+        <a className={`species-icon`} style={{fontSize: `600%`, borderBottom: 0}} href={description.url}>
+          <EbiSpeciesIcon species={iconSrc}/>
+        </a> :
+        <span className={`species-icon`} style={{fontSize: `600%`}}>
+          <EbiSpeciesIcon species={iconSrc}/>
         </span>
+    }
 
-      }
+    {
+      description &&
+      <h5 className={`species-name`}>
+        {
+          description.url ?
+            <a className={`species-url`} href={description.url}>{description.text}</a> :
+            description.text
+        }
+      </h5>
+    }
+
+    {
+      content &&
+      <span className={`content`}>
+        {
+          content.map((item) =>
+            item.url ?
+              <p className={`url`} style={{marginBottom: `5px`}} key={item.text}>
+                <a href={item.url}>{item.text}</a>
+              </p> :
+              <p className={`text`} style={{marginBottom: `5px`}} key={item.text}>{item.text}</p>
+          )
+        }
+      </span>
+    }
 
 
-    </div>
-  )
-}
+  </div>
 
 SpeciesCard.propTypes = cardPropTypes
 
