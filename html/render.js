f78b2dd846b518ad122a123ab544671526902dd5
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-11-14 14:46:32 +0000

    More complete demo page, with tons of examples

--- html/render.js
+++ html/render.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import CardContainer from '../src/index.js'
+import CardContainer from '../src/index'
+import SpeciesCard from '../src/cards/SpeciesCard'
+import ExtendableSpeciesCard from '../src/cards/ExtendableSpeciesCard'
+import ExtendableImageCard from '../src/cards/ExtendableImageCard'
 
 const render = (options, target) => {
   ReactDOM.render(<CardContainer {...options} />, document.getElementById(target))
 }
 
-export {render}
+const renderSpeciesCard = (options, target) => {
+  ReactDOM.render(<SpeciesCard {...options} />, document.getElementById(target))
+}
+
+const renderExtendableSpeciesCard = (options, target) => {
+  ReactDOM.render(<ExtendableSpeciesCard {...options} />, document.getElementById(target))
+}
+
+const renderExtendableImageCard = (options, target) => {
+  ReactDOM.render(<ExtendableImageCard {...options} />, document.getElementById(target))
+}
+
+export { render, renderSpeciesCard, renderExtendableSpeciesCard, renderExtendableImageCard }
