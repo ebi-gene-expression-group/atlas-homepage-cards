aed716cae5b7ef34d1f81eebba1449eccaae0de1
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2018-11-15 01:44:10 +0000

    Use canned props from TestUtils

--- __test__/SpeciesCard.test.js
+++ __test__/SpeciesCard.test.js
@@ -5,6 +5,7 @@ import { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import EbiSpeciesIcon from 'react-ebi-species'
 
+import { getRandomInt } from './TestUtils'
 import { batmanFilmsSpeciesCardProps, findingNemoSpeciesCardProps } from './TestUtils'
 import SpeciesCard from '../src/cards/SpeciesCard'
 
@@ -12,82 +13,43 @@ Enzyme.configure({ adapter: new Adapter() })
 
 describe(`SpeciesCard`, () => {
   test.each([ [batmanFilmsSpeciesCardProps.description.text, batmanFilmsSpeciesCardProps],
-              [findingNemoSpeciesCardProps.description.text,  findingNemoSpeciesCardProps] ])(
+              [findingNemoSpeciesCardProps.description.text, findingNemoSpeciesCardProps] ])(
     `matches snapshot: %s`, (titleText, props) => {
     const tree = renderer.create(<SpeciesCard {...props}/>).toJSON()
     expect(tree).toMatchSnapshot()
   })
 
-  test(`does not render optional empty content`, () => {
-    const wrapper =
-      shallow(
-        <SpeciesCard
-          iconType={`species`}
-          iconSrc={``} />)
+  test(`does not render optional empty title and content`, () => {
+    const wrapper = shallow(<SpeciesCard iconType={`species`} iconSrc={``} />)
 
     expect(wrapper.find(EbiSpeciesIcon).exists()).toBe(true)
+    expect(wrapper.find(`h5`).exists()).toBe(false)
     expect(wrapper.find(`li`).exists()).toBe(false)
     expect(wrapper.find(`ul`).exists()).toBe(false)
   })
 
   test(`does not render non-existent URLs`, () => {
-    const speciesName = `Mus musculus`
-    const content = [
-      {
-        text: `120 experiments`
-      },
-      {
-        text: `1 baseline experiment`
-      },
-      {
-        text: `119 differential experiments`
-      }
-    ]
+    const props = findingNemoSpeciesCardProps
+    const wrapper = shallow(<SpeciesCard {...props} />)
 
-    const description = {
-      text: speciesName
-    }
-
-    const wrapper =
-      shallow(
-        <SpeciesCard
-          iconType={`species`}
-          iconSrc={speciesName}
-          content={content}
-          description={description}/>)
     expect(wrapper.find(`h5`).exists()).toBe(true)
-    expect(wrapper.find(`h5`).text()).toBe(speciesName)
-    expect(wrapper.find(`h5 a`).exists()).toBe(false)
+    expect(wrapper.find(`h5`).text()).toBe(props.description.text)
+    expect(wrapper.find(`h5 a`).exists()).toBe(Boolean(props.description.url))
 
     const contentWrapper = wrapper.find(`ul`)
 
     expect(contentWrapper.exists()).toBe(true)
-    expect(contentWrapper.find(`li`)).toHaveLength(3)
-    expect(contentWrapper.find(`li a`).exists()).toBe(false)
+    expect(contentWrapper.find(`li`)).toHaveLength(props.content.length)
+    expect(contentWrapper.find(`li a`).exists()).toBe(Boolean(props.content[0].url))
   })
 
   test(`renders URLs`, () => {
-    const description = {
-      text: `Mus musculus`,
-      url: `http://foo.bar/species`
-    }
+    const props = batmanFilmsSpeciesCardProps
+    props.description.url = `#`
 
-    const content = [
-      {
-        text: `120 experiments`,
-        url: `http://example.com/experiments`
-      }
-    ]
-
-    const wrapper =
-      shallow(
-        <SpeciesCard
-          iconType={`species`}
-          iconSrc={``}
-          content={content}
-          description={description}/>)
+    const wrapper = shallow(<SpeciesCard {...props}/>)
 
     expect(wrapper.find(`h5 a`).exists()).toBe(true)
-    expect(wrapper.find(`li a`)).toHaveLength(1)
+    expect(wrapper.find(`li a`)).toHaveLength(props.content.length)
   })
 })
