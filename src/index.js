5044ecc3b7a5d2bb31cb44f91badef9b8341d92e
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-30 11:01:09 +0000

    Add a latest experiment card container for sc home

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import withFetchLoader from './containers/FetchLoader'
 import SceaHomepageSpeciesContainer from './containers/SceaHomepageSpeciesContainer'
 import HcaLandingPageContainer from './containers/HcaLandingPageContainer'
+import SceaHomePageLatestExperimentContainer from './containers/SceaHomePageLatestExperimentContainer'
 
 const _SceaHomepageSpeciesContainer = withFetchLoader(SceaHomepageSpeciesContainer)
 const _HcaLandingPageContainer = withFetchLoader(HcaLandingPageContainer)
+const _SceaHomePageLatestExperimentContainer = withFetchLoader(SceaHomePageLatestExperimentContainer)
 
 export {
   _SceaHomepageSpeciesContainer as SceaHomepageSpeciesContainer,
-  _HcaLandingPageContainer as HcaLandingPageContainer
+  _HcaLandingPageContainer as HcaLandingPageContainer,
+  _SceaHomePageLatestExperimentContainer as SceaHomePageLatestExperimentContainer
 }
