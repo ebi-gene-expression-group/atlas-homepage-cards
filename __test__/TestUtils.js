4de89db7e2b23a5a51373fe98d8f87a94ca4d620
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-30 14:01:43 +0000

    Add a test for latest experiment container and …
    a demo json payload

--- __test__/TestUtils.js
+++ __test__/TestUtils.js
@@ -150,8 +150,37 @@ const findingNemoSpeciesCardProps = {
   ]
 }
 
+const latestExperimentCardsProps = [
+  {
+    experimentType: `SINGLE_CELL_RNASEQ_MRNA_BASELINE`,
+    experimentAccession: `E-EHCA-2`,
+    experimentDescription: `Melanoma infiltration of stromal and immune cells`,
+    lastUpdate: `16-11-2018`,
+    numberOfAssays: 6638,
+    numberOfContrasts: 0,
+    species: `Mus musculus`,
+    kingdom: `animals`,
+    experimentalFactors: [`single cell identifier`, `sampling site`, `time`],
+    arrayDesigns: [],
+    arrayDesignNames: []
+  },
+  {
+    experimentType: `SINGLE_CELL_RNASEQ_MRNA_BASELINE`,
+    experimentAccession: `E-MTAB-5061`,
+    experimentDescription: `Single-cell RNA-seq analysis`,
+    lastUpdate: `11-10-2018`,
+    numberOfAssays: 3514,
+    numberOfContrasts: 0,
+    species: `Homo sapiens`,
+    kingdom: `animals`,
+    experimentalFactors: [`single cell identifier`, `disease`],
+    arrayDesigns: [],
+    arrayDesignNames:[]
+  }
+]
 export {
   getRandomInt,
   aRickleInTimeImageCardProps, theSmithHouseholdImageCardProps,
-  batmanFilmsSpeciesCardProps, findingNemoSpeciesCardProps
+  batmanFilmsSpeciesCardProps, findingNemoSpeciesCardProps,
+  latestExperimentCardsProps
 }
