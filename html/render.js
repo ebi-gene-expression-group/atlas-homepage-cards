ce699f76c8fafed7f5933165bfaeb0a632e1590f
alfonsomunozpomer <amunoz@ebi.ac.uk>
2019-03-12 01:04:15 +0000

    Decouple component from FetchLoader and add it as a dev dependency

--- html/render.js
+++ html/render.js
@@ -2,16 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { SceaHomepageSpeciesContainer, HcaLandingPageContainer } from '../src/index'
+import withFetchLoader from 'atlas-react-fetch-loader'
 
 import SpeciesCard from '../src/cards/SpeciesCard'
 import ExtendableCard from '../src/cards/ExtendableCard'
 
+const FetchLoadSceaHomepageSpeciesContainer = withFetchLoader(SceaHomepageSpeciesContainer)
+const FetchLoadHcaLandingPageContainer = withFetchLoader(HcaLandingPageContainer)
+
 const renderSceaHomepageSpeciesContainer = (options, target) => {
-  ReactDOM.render(<SceaHomepageSpeciesContainer {...options} />, document.getElementById(target))
+  ReactDOM.render(<FetchLoadSceaHomepageSpeciesContainer {...options} />, document.getElementById(target))
 }
 
 const renderHcaLandingPageContainer = (options, target) => {
-  ReactDOM.render(<HcaLandingPageContainer {...options} />, document.getElementById(target))
+  ReactDOM.render(<FetchLoadHcaLandingPageContainer {...options} />, document.getElementById(target))
 }
 
 const renderSpeciesCard = (options, target) => {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import withFetchLoader from './containers/FetchLoader'
 import SceaHomepageSpeciesContainer from './containers/SceaHomepageSpeciesContainer'
 import HcaLandingPageContainer from './containers/HcaLandingPageContainer'
 
-const _SceaHomepageSpeciesContainer = withFetchLoader(SceaHomepageSpeciesContainer)
-const _HcaLandingPageContainer = withFetchLoader(HcaLandingPageContainer)
-
-export {
-  _SceaHomepageSpeciesContainer as SceaHomepageSpeciesContainer,
-  _HcaLandingPageContainer as HcaLandingPageContainer
-}
+export { SceaHomepageSpeciesContainer, HcaLandingPageContainer }
