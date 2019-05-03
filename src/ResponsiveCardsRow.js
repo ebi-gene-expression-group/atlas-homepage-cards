a5c3b3842c2ecd0bcef28521da875303d7127c06
lingyun1010 <zhaolingyun1010@gmail.com>
2019-05-03 11:44:27 +0100

    Wrap card container with slide if enabled and set
    default style for slide display

--- src/ResponsiveCardsRow.js
+++ src/ResponsiveCardsRow.js
@@ -4,10 +4,13 @@ import styled from 'styled-components'
 
 import cardPropTypes from './modelPropTypes'
 import Card from './Card'
+import Slide from 'react-slick'
+import "./slick.css"
 
 const ResponsiveCardsRow = (props) => {
-  const { CardClass, cards } = props
+  const { CardClass, cards, isSlider, slideSettings } = props
   const { className, cardContainerClassName, speciesIconHeight, imageIconHeight, hoverColour } = props
+  slideSettings.slidesToShow = slideSettings.slidesToShow >= cards.length ? cards.length : slideSettings.slidesToShow
 
   const CardContainer = styled.div`
     /* border-radius: 8px; */
@@ -15,20 +18,21 @@ const ResponsiveCardsRow = (props) => {
       background: ${hoverColour};
     }
   `
-
+  const cardsDisplay = Array.isArray(cards) && cards.map((card, index) =>
+    <CardContainer className={cardContainerClassName} key={index}>
+      <CardClass
+        {...card}
+        imageIconHeight={imageIconHeight}
+        speciesIconHeight={speciesIconHeight}/>
+    </CardContainer>)
   return (
     <div className={className}>
-      {
-        Array.isArray(cards) &&
-        cards.map((card, index) =>
-          <CardContainer className={cardContainerClassName} key={index}>
-            <CardClass
-              {...card}
-              imageIconHeight={imageIconHeight}
-              speciesIconHeight={speciesIconHeight}/>
-          </CardContainer>
-        )
-      }
+      { isSlider ?
+        <Slide id={`slide`} 
+          {...slideSettings}>
+          {cardsDisplay}
+        </Slide> : 
+        cardsDisplay }
     </div>
   )
 }
@@ -40,16 +44,28 @@ ResponsiveCardsRow.propTypes = {
   cardContainerClassName: PropTypes.string,
   speciesIconHeight: PropTypes.string,
   imageIconHeight: PropTypes.string,
-  hoverColour: PropTypes.string
+  hoverColour: PropTypes.string,
+  isSlider: PropTypes.bool,
+  slideSettings: PropTypes.object
 }
 
 ResponsiveCardsRow.defaultProps = {
   CardClass: Card,
+  isSlider: false,
   className: ``,
   cardContainerClassName: ``,
   speciesIconHeight: `6rem`,
   imageIconHeight: `2rem`,
-  hoverColour: `AliceBlue`
+  hoverColour: `AliceBlue`,
+  slideSettings: {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  }
 }
 
 export default ResponsiveCardsRow
