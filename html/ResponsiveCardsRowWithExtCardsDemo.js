b8fe197498d46eddd1c0d97d00aa0f30ac3f7371
lingyun1010 <zhaolingyun1010@gmail.com>
2019-05-03 11:44:51 +0100

    Pass slide settings as props to demo

--- html/ResponsiveCardsRowWithExtCardsDemo.js
+++ html/ResponsiveCardsRowWithExtCardsDemo.js
@@ -6,10 +6,46 @@ import withFetchLoader from 'atlas-react-fetch-loader'
 
 const FetchLoadResponsiveCardsRow = withFetchLoader(ResponsiveCardsRow)
 
+const slideSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+}
+
 const render = (options, target) => {
   ReactDOM.render(
     <FetchLoadResponsiveCardsRow
       CardClass={ExtendableCard}
+      slideSettings={slideSettings}
       {...options}
     />,
     document.getElementById(target))
