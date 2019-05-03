435757ec00867ffc3a0e15df3001d543dec3372d
lingyun1010 <zhaolingyun1010@gmail.com>
2019-05-03 11:45:58 +0100

    Update test with slide component requires  define
    matchMedia for react-slick

--- __test__/ResponsiveCardsRow.test.js
+++ __test__/ResponsiveCardsRow.test.js
@@ -1,4 +1,5 @@
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
+import matchMedia from './matchMedia'
 
 import {
   aRickleInTimeImageCardProps, findingNemoSpeciesCardProps,     // URL in title, no URLs in content
@@ -15,10 +16,21 @@ describe(`ResponsiveCardsRow`, () => {
         batmanFilmsSpeciesCardProps,
         findingNemoSpeciesCardProps,
         theSmithHouseholdImageCardProps
-      ]
+      ],
+    CardClass: () => `topClass`
   }
 
-  test(`matches snapshot`, () => {
-    expect(mount(<ResponsiveCardsRow {...props}/>)).toMatchSnapshot()
+  test(`displays cards in default row mode`, () => {
+    const wrapper = shallow(<ResponsiveCardsRow {...props}/>)
+    expect(wrapper.find(`#slide`)).toHaveLength(0)
+  })
+
+  test(`displays cards in slider mode if isSlider is true`, () => {
+    const wrapper = shallow(<ResponsiveCardsRow {...props} isSlider={true}/>)
+    expect(wrapper.find(`#slide`)).toHaveLength(1)
+  })
+
+  test(`displays all cards`, () => {
+    expect(shallow(<ResponsiveCardsRow {...props}/>).find(props.CardClass)).toHaveLength(props.cards.length)
   })
 })
